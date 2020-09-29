e42822fb45e878ad2013cbde3acaf36ae66a3e54
Hanspeter Schmid <hanspeter.schmid@fhnw.ch>
2020-09-29 15:31:16 +0200

    Last push failed, retry

--- src/signalflowgrapher/gui/mason_window.py
+++ src/signalflowgrapher/gui/mason_window.py
@@ -28,7 +28,8 @@ class MasonWindow(QDialog, mason_window_ui):
                 lambdarepr(interim_res.transfer_function))
         }
 
-        # Create full formula
+        # Create full formula; the non-simplified versdion serves to 
+        # derive the symbol list below
         T = interim_res.transfer_function[0][0]
         non_simplified = T.subs(interim_res.transfer_function) \
             .subs(interim_res.numerator) \
