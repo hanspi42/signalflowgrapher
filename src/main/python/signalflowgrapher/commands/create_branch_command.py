b7255964c167fcd67d93b4a015480762c1d9cb4f
gitmisa <231751208+gitmisa@users.noreply.github.com>
2025-11-27 10:19:57 +0100

    Enhance command classes (create/remove) to include grid position management during undo/redo operations:  This fixes the bug that the position of deleted or not created nodes and branches is not updated (when the grid is moved between undo/redo)

--- src/main/python/signalflowgrapher/commands/create_branch_command.py
+++ src/main/python/signalflowgrapher/commands/create_branch_command.py
@@ -1,18 +1,30 @@
 from signalflowgrapher.commands.command_handler import Command
-from signalflowgrapher.model.model import ObservableGraph, CurvedBranch
+from signalflowgrapher.model.model import ObservableGraph, CurvedBranch, Model
 
 
 class CreateBranchCommand(Command):
-    def __init__(self, graph: ObservableGraph, branch: CurvedBranch):
+    def __init__(self, graph: ObservableGraph, branch: CurvedBranch, model: Model):
         self.__graph = graph
         self.__branch = branch
         self.__start_node = branch.start
         self.__end_node = branch.end
+        self.__model = model
+        self.__gridpos_atCreation = self.__model.get_grid_position()
 
     def undo(self):
         # Remove branch from nodes and graph
         self.__branch.remove()
+        # Grid position reset (No position correction is necessary as long as the node is not removed.)
+        self.__gridpos_atCreation = self.__model.get_grid_position()
 
     def redo(self):
+        # Restore node position
+        gridpos_new = self.__model.get_grid_position()
+        dx = gridpos_new[0] - self.__gridpos_atCreation[0]
+        dy = gridpos_new[1] - self.__gridpos_atCreation[1]
+        self.__branch.transform(dx, dy, dx, dy)
+        self.__gridpos_atCreation = gridpos_new
+
         # Reconnect branch
         self.__branch.reconnect(self.__start_node, self.__end_node)
+
