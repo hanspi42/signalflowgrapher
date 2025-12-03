569614ff0a8b727d24f36532ccdfa23a29241e63
gitmisa <231751208+gitmisa@users.noreply.github.com>
2025-12-03 15:13:41 +0100

    Refactor command tests to include model reference for CreateBranchCommand, CreateNodeCommand, RemoveBranchCommand, and RemoveNodeCommand

--- src/main/python/signalflowgrapher/tests/commands/test_create_branch_command.py
+++ src/main/python/signalflowgrapher/tests/commands/test_create_branch_command.py
@@ -2,16 +2,17 @@ import unittest
 from unittest.mock import MagicMock, Mock
 from signalflowgrapher.commands.create_branch_command \
     import CreateBranchCommand
-from signalflowgrapher.model.model import ObservableGraph
+from signalflowgrapher.model.model import ObservableGraph, Model
 
 
 class TestCreateBranchCommand(unittest.TestCase):
     def test_undo(self):
         # Prepare
-        model_mock = MagicMock(ObservableGraph)
+        graph_mock = MagicMock(ObservableGraph)
         branch_mock = Mock()
+        model_mock = MagicMock(Model)
 
-        command = CreateBranchCommand(model_mock, branch_mock)
+        command = CreateBranchCommand(graph_mock, branch_mock, model_mock)
 
         # Execute
         command.undo()
@@ -21,14 +22,15 @@ class TestCreateBranchCommand(unittest.TestCase):
 
     def test_redo(self):
         # Prepare
-        model_mock = MagicMock(ObservableGraph)
+        graph_mock = MagicMock(ObservableGraph)
         branch_mock = Mock()
         start_node_mock = Mock()
         end_node_mock = Mock()
+        model_mock = MagicMock(Model)
         branch_mock.start = start_node_mock
         branch_mock.end = end_node_mock
 
-        command = CreateBranchCommand(model_mock, branch_mock)
+        command = CreateBranchCommand(graph_mock, branch_mock, model_mock)
 
         # Execute
         command.redo()
--- src/main/python/signalflowgrapher/tests/commands/test_create_node_command.py
+++ src/main/python/signalflowgrapher/tests/commands/test_create_node_command.py
@@ -1,26 +1,29 @@
 import unittest
 from unittest.mock import MagicMock, Mock
 from signalflowgrapher.commands.create_node_command import CreateNodeCommand
-from signalflowgrapher.model.model import ObservableGraph
+from signalflowgrapher.model.model import ObservableGraph, Model
 
 
 class TestCreateNodeCommand(unittest.TestCase):
     def test_undo(self):
-        model_mock = MagicMock(ObservableGraph)
+        graph_mock = MagicMock(ObservableGraph)
         node_mock = Mock()
+        model_mock = MagicMock(Model)
 
-        command = CreateNodeCommand(model_mock, node_mock)
+
+        command = CreateNodeCommand(graph_mock, node_mock, model_mock)
         command.undo()
 
         # Assert
         node_mock.remove.assert_called_once()
 
     def test_redo(self):
-        model_mock = MagicMock(ObservableGraph)
+        graph_mock = MagicMock(ObservableGraph)
         node_mock = Mock()
+        model_mock = MagicMock(Model)
 
-        command = CreateNodeCommand(model_mock, node_mock)
+        command = CreateNodeCommand(graph_mock, node_mock, model_mock)
         command.redo()
 
         # Assert
-        model_mock.add_node.assert_called_once_with(node_mock)
+        graph_mock.add_node.assert_called_once_with(node_mock)
